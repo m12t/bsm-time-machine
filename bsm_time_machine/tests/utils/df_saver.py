5c8a3074000aa7348bf0115379a528336adebe4f
Michael Willett <51060085+m12t@users.noreply.github.com>
2023-05-28 18:42:08 -0500

    file for generating and saving test case dataframe pickles

---
+++ bsm_time_machine/tests/utils/df_saver.py
@@ -0,0 +1,42 @@
+"""
+a util file for generating test dataframe pickles.
+
+example invocation: python utils/df_saver.py --dir filter_days --name 100_trading_days --is_case True
+"""
+import argparse
+import os
+
+import pandas as pd
+import numpy as np
+
+
+def main(name: str, dir: str, is_case: bool):
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    os.chdir(f"../data/{dir}")
+    prefix = "cases/" if is_case else "expected/"
+    df = generate_df()
+    if df is None:
+        print("aborted")
+        return
+    df.to_pickle(prefix + name + ".pkl")
+
+
+def generate_df() -> pd.DataFrame():
+    """modify this code to chage what the df should look like..."""
+    df = pd.DataFrame(np.resize(np.arange(5), 100), columns=["day"])
+    print(df)
+    if input("does the below df look correct? [y/n]:") != "y":
+        return
+    return df
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="df_saver",
+        description="",
+    )
+    parser.add_argument("--name", type=str)
+    parser.add_argument("--dir", type=str, required=True)
+    parser.add_argument("--is_case", type=bool, required=True)
+    args = parser.parse_args()
+    main(args.name, args.dir, args.is_case)
